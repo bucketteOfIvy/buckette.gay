c7062ec313a0ba24287844cf0873d75ee2ab2bdc
bucketteOfIvy <ashewimer@gmail.com>
2022-06-19 13:26:25 -0500

    hopefully fixed the map issue

--- js/map.js
+++ js/map.js
@@ -3,15 +3,14 @@
 // load in a basemap from OSM of Hyde Park at it's latitude and longitude
 var map = L.map('map').setView([41.7898, -87.5987], 16)
 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   maxZoom: 19,
-//   attribution: '© OpenStreetMap'
-// }).addTo(map) // add it to the map
-
-L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-  maxZoom: 20,
-  attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo(map)
+L.tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+// L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+//   maxZoom: 20,
+//   attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+// }).addTo(map)
 
 var style = {
   fillColor: '#F7A8B8',
